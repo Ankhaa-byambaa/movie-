76eded9ec587fcf6e73cdb661a67a7b27255c6b6
25LP5026 25LP5026 <25LP5026@G301-CC0207.local>
2025-09-08 13:23:16 +0800

    a

---
+++ src/components/home/card1.tsx
@@ -0,0 +1,40 @@
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+type card1Type = {
+  score: number;
+  title: string;
+  image: string;
+};
+export function Card1({ score, title, image }: card1Type) {
+  return (
+    <>
+      <Card
+        className={
+          "w-[230px] bg-secondary p-0 overflow-hidden gap-2 hover:bg-[#0000004D] h-[440px]"
+        }
+      >
+        <CardContent className={"p-0 "}>
+          <div className=" bg-black h-[340px]">
+            <img className="w-[230px]  hover:opacity-50" src={image} />
+          </div>
+        </CardContent>
+
+        <CardFooter className={"flex flex-col items-start p-2 "}>
+          <CardDescription className={"flex gap-2"}>
+            <span className="test-3">⭐{score}/10</span>
+          </CardDescription>
+          <CardTitle className="text-[18px] text-[#09090B] text-semibold  ">
+            {title}
+          </CardTitle>
+        </CardFooter>
+      </Card>
+    </>
+  );
+}

0fc960bb3d5db52c848e3fa34be7a57dcf4eb198
25lp5026 <25lp5026@G301-CC0193.local>
2025-12-18 11:42:15 +0800

    q

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { HomePage } from "@/components/home/HomePage";
 import { HomePageSkeleton } from "@/components/home/HomePageSkeleton";
 import { Suspense } from "react";
 
-export default function Home() {
+export default function NotFound() {
   return (
     <Suspense fallback={<HomePageSkeleton />}>
       <HomePage />
